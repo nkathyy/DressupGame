9f3bfa372d74fd3fdd39119b205fb74a3d5b6068
Kathy <kathyaudrey2001@gmail.com>
2025-07-16 00:18:55 +0800

    tab list info

---
+++ src/utils/tabsList.js
@@ -0,0 +1,18 @@
+export const tabsList = [
+    {
+        index: 1,
+        name: "Hair",
+    },
+    {
+        index: 2,
+        name: "Face",
+    },
+    {
+        index: 3,
+        name: "Clothes",
+    },
+    {
+        index: 4,
+        name: "Accessory",
+    },
+]
