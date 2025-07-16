71dde9e7aa290f89688df79d5fc58d6f1c8be0d2
Kathy <kathyaudrey2001@gmail.com>
2025-07-17 00:30:33 +0800

    result image placing test

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,5 +1,11 @@
 import "./Result.css";
 
 export default function Result() {
-  return <div className="assets"></div>;
+  return (
+    <div className="assets">
+      <img src={require("../assets/img/F0.png")}></img>
+      <img src={require("../assets/img/C0.png")}></img>
+      <img src={require("../assets/img/H0.png")}></img>
+    </div>
+  );
 }
