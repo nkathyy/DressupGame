86f006596ae1853e48489cf9af8e26b14f897c49
Kathy <kathyaudrey2001@gmail.com>
2025-07-17 00:31:45 +0800

    add alt to img

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,9 +3,9 @@ import "./Result.css";
 export default function Result() {
   return (
     <div className="assets">
-      <img src={require("../assets/img/F0.png")}></img>
-      <img src={require("../assets/img/C0.png")}></img>
-      <img src={require("../assets/img/H0.png")}></img>
+      <img src={require("../assets/img/F0.png")} alt="face"></img>
+      <img src={require("../assets/img/C0.png")} alt="clothes"></img>
+      <img src={require("../assets/img/H0.png")} alt="hair"></img>
     </div>
   );
 }
