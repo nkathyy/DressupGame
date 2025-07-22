090c3bde204bf8c59795373c4e3815f0e07583ee
Kathy <kathyaudrey2001@gmail.com>
2025-07-23 01:07:38 +0800

    display result according to chosen items

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,11 +1,14 @@
 import "./Result.css";
 
-export default function Result() {
+export default function Result({ currentElement }) {
   return (
     <div className="assets">
-      <img src={"/assets/img/F0.png"} alt="face"></img>
-      <img src={"/assets/img/C0.png"} alt="clothes"></img>
-      <img src={"/assets/img/H0.png"} alt="hair"></img>
+      <img src={`/assets/img/${currentElement.Face}.png`} alt="face"></img>
+      <img
+        src={`/assets/img/${currentElement.Clothes}.png`}
+        alt="clothes"
+      ></img>
+      <img src={`/assets/img/${currentElement.Hair}.png`} alt="hair"></img>
     </div>
   );
 }
