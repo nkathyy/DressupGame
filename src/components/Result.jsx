1397162ade65b383b3a15b6909489176222fec83
Kathy <kathyaudrey2001@gmail.com>
2025-08-01 14:29:14 +0800

    update image path and change to display result in canvas instead of img

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,16 +1,51 @@
 import "./Result.css";
+import { img } from "../constants/imgMap.js";
+import { useEffect, useRef } from "react";
+
+const loadImage = (src) =>
+  new Promise((resolve) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.src = src;
+  });
 
 export default function Result({ currentItems }) {
+  const resultRef = useRef(null);
+
+  const drawCanvas = async () => {
+    const canvas = resultRef.current;
+    const context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    const images = await Promise.all(
+      Object.values(currentItems).map((value) => loadImage(img[`${value}`]))
+    );
+
+    images.forEach((img) => {
+      context.drawImage(img, 0, 0, canvas.width, canvas.height);
+    });
+
+    context.drawImage(
+      await loadImage(img["watermark"]),
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+  };
+
+  useEffect(() => {
+    drawCanvas();
+  }, [currentItems]);
+
   return (
     <div className="assets">
-      <img src={`/assets/img/${currentItems.Face}.png`} alt="face"></img>
-      <img src={`/assets/img/${currentItems.Clothes}.png`} alt="clothes"></img>
-      <img src={`/assets/img/${currentItems.Hair}.png`} alt="hair"></img>
-      <img
-        src={`/assets/img/${currentItems.Accessory}.png`}
-        alt="accessory"
-      ></img>
-      <img src={`/assets/img/watermark.png`} alt="watermark"></img>
+      <canvas
+        ref={resultRef}
+        width="600px"
+        height="750px"
+        className="resultCanvas"
+      ></canvas>
     </div>
   );
 }
