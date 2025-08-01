bd2553b79b6e78734d1f10295d6de55a95fb0de5
Kathy <kathyaudrey2001@gmail.com>
2025-08-01 14:28:32 +0800

    update img position and add js for image mapping

---
+++ src/constants/imgMap.js
@@ -0,0 +1,14 @@
+import watermark from "../assets/img/watermark.png"
+import A0 from "../assets/img/A0.png"
+import C0 from "../assets/img/C0.png"
+import F0 from "../assets/img/F0.png"
+import F1 from "../assets/img/F1.png"
+import F2 from "../assets/img/F2.png"
+import H0 from "../assets/img/H0.png"
+import H1 from "../assets/img/H1.png"
+import H2 from "../assets/img/H2.png"
+import H3 from "../assets/img/H3.png"
+
+export const img ={
+    watermark, A0,  C0, F0, F1, F2, H0, H1, H2, H3
+}
