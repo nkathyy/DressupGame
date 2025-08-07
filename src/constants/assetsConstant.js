a8528cd87f4199acb9da8f15fddd7bdc3ab6149a
Kathy <kathyaudrey2001@gmail.com>
2025-08-07 15:43:55 +0800

    update assets

--- src/constants/assetsConstant.js
+++ src/constants/assetsConstant.js
@@ -1,4 +1,4 @@
-export const FACE_LENGTH = 3;
-export const CLOTHES_LENGTH = 1;
-export const HAIR_LENGTH = 4; 
+export const FACE_LENGTH = 12;
+export const CLOTHES_LENGTH = 13;
+export const HAIR_LENGTH = 15; 
 export const ACCESSORY_LENGTH =1;
